b9b998341b347b6748ea8ba5389cb503d5ee47ae
NiyiAyo <tohlhuh@gmail.com>
2022-09-13 17:11:54 +0100

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,18 +1,20 @@
 #include "main.h"
 
 /**
- * main - The main function is the entry point of this and all c programs
- * Return: 0
+ * main - print putchar
+ * Return: always 0
  */
 
 int main(void)
 {
-	_putchar("_\n");
-	_putchar("p\n");
-	_putchar("u\n");
-	_putchar("t\n");
-	_putchar("c\n");
-	_putchar("h\n");
-	_putchar("a\n");
-	_putchar("r\n");
+	_putchar("_");
+	_putchar("p");
+	_putchar("u");
+	_putchar("t");
+	_putchar("c");
+	_putchar("h");
+	_putchar("a");
+	_putchar("r");
+	_putchar("\n");
+	return (0);
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,8 +1,9 @@
-#ifndef HOLBERTON_H_INCLUDED;
-#define HOLBERTON_H_INCLUDED;
+#ifndef MASTER
+#define MASTER
 
-int _putchar(char c);
+int _putchar(char c)
 void print_alphabet(void);
+void print_alphabet_x10(void);
 int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
@@ -13,6 +14,6 @@ void times_table(void);
 int add(int a, int b);
 void print_to_98(int n);
 void print_times_table(int n);
-void num(int r, int n);
+void num(int r, int n)
 
 #endif
