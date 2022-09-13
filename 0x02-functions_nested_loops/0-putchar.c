065ff311b18be3b19f67adf906ab0855482fc340
NiyiAyo <tohlhuh#gmail.com>
2022-09-13 16:32:12 +0100

    Necessary Files

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
  * main - The main function is the entry point of this and all c programs
@@ -7,5 +7,12 @@
 
 int main(void)
 {
-	printf("_putchar\n");
+	_putchar("_\n");
+	_putchar("p\n");
+	_putchar("u\n");
+	_putchar("t\n");
+	_putchar("c\n");
+	_putchar("h\n");
+	_putchar("a\n");
+	_putchar("r\n");
 }
