1a6d3a939d2470ed9ca01731667b06b4e650d598
NiyiAyo <tohlhuh@gmail.com>
2022-09-19 23:35:14 +0100

    Task #0 completed

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * reset_to_98 - function that changes the value of a pointer to zero
+ * Return: 0
+ * n - variable pointer
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
