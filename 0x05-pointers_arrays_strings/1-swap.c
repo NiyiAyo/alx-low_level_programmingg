7b04377906e1caaed583741fdbfe8f3ca1c696cf
NiyiAyo <tohlhuh@gmail.com>
2022-09-19 23:58:25 +0100

    Task #1 finished

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -3,13 +3,11 @@
  * swap_int- function thata swaps values of two integers
  * @a: variable 1
  * @b: variable 2
- * @c: variable 3
+ *
  */
 void swap_int(int *a, int *b)
 {
-	int c;
-
-	c = *a;
-	*a = *b;
-	*b = c;
+	*a += *b;
+	*b = *a - *b;
+	*a = *a - *b;
 }
